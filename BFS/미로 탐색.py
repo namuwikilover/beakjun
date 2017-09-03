285575fcba0944a9862e66463771666e12d3261b
tjdgns8047 <tjdgns8047@naver.com>
2017-09-03 23:44:55 +0900

    미로 탐색 품!

--- BFS/미로 탐색.py
+++ BFS/미로 탐색.py
@@ -1,12 +1,14 @@
-def bfs(start, maze):
+import sys;
+
+def bfsSearch(maze, N, M):
     queue = [];
     cur = 0;
 
     xDirection = [0, 1, 0, -1];
-    yDirection = [-1, 0, -1, 0];
+    yDirection = [1, 0, -1, 0];
 
     queue.append([0, 0]);
-    matrix[0][0] = -1;
+    maze[0][0] = -1;
 
     while queue:
         xCurrent, yCurrent = map(int, queue.pop(0));
@@ -15,14 +17,16 @@ def bfs(start, maze):
             xNext = xCurrent + xDirection[i];
             yNext = yCurrent + yDirection[i];
 
-            if (xNext > 0 and xNext < N) and (yNext > 0 and yNext < M):
-                maze[xNext][yNext] = maze[xCurrent][yCurrent] - 1;
+            if (xNext >= 0 and xNext < N) and (yNext >= 0 and yNext < M):
+                if maze[xNext][yNext] == 1:
+                    maze[xNext][yNext] = maze[xCurrent][yCurrent] - 1;
+                    queue.append([xNext, yNext]);
 
-            if nextDx==N and nextDy == M:
-                print('Found!!!');
-                break;
+            if xNext == N-1 and yNext == M-1:
+                print(abs(maze[xNext][yNext]));
+                return;
 
-            queue.append([xNext, yNext]);
+    print(maze)
 
 def main():
     N, M = map(int, input().split())
@@ -30,7 +34,11 @@ def main():
     l = []
 
     for i in range(N):
-        l.append(input())
+        l.append(list(map(int, input())))
+
+    #print(l)
+
+    bfsSearch(l, N, M);
     
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
